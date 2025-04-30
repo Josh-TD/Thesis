1955bd41b3e9e04b49758e95b1672ffdbf960f45
Josh Daniel <joshtdaniel@gmail.com>
2025-04-30 02:32:37 -0400

    Trying to host

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../assets/logo.png'; // adjust the path if needed
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/Dialogue">Dialogue</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Dialogue.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dialogue = () => {
+  return <h2>Dialogue Section</h2>;
+};
+
+export default Dialogue;
